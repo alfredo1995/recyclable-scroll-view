cb0121bf9cdbd9dce4db0bd4c6a702e03cdcdced
Alfredo Gomes Pereira <71193893+alfredo1995@users.noreply.github.com>
2024-05-09 00:04:59 -0300

    mecanismo de reciclagem (Object Pooling)
    Implementação de um mecanismo de reciclagem (Object Pooling) para remover elementos de cores recicladas da área visível em uma visualização de rolagem.
    
    Falta para calcular a área visível (altura 290) da visualização de rolagem é definir sua altura e posição superior, enquanto a parte inferior coincide com a soma da posição superior e a altura da visualização (altura total do contêiner 2000

---
+++ Assets/Scripts/DetectVisibility.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectVisibility : MonoBehaviour
+{
+    [SerializeField] ColotList colotList;
+    [SerializeField] List<Image> obj = new List<Image>();
+    [SerializeField] GameObject objPrefab;
+
+    [SerializeField] RectTransform content;
+    [Space]
+
+    [SerializeField] Canvas canvas;
+    public ScrollRect scrollView;
+    public RectTransform itemRectTransform;
+
+
+    private void Awake()
+    {
+        int t = 0;
+
+        if (colotList.colors.Count > 10)
+            t = 10;
+        else
+            t = colotList.colors.Count;
+
+        for (int i = 0; i < t; i++)
+        {
+            Image img = Instantiate(objPrefab, content).GetComponent<Image>();
+            img.color = colotList.colors[i];
+            img.GetComponent<ItemIndex>().colotList = colotList;
+            img.GetComponent<ItemIndex>().id = i;
+
+            obj.Add(img);
+        }
+    }
+}
